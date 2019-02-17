8105830c4f289ea823e59523ae080fbc23cbdaa3
ShootHzj <shoothzj@gmail.com>
2019-02-17 18:08:08 +0800

    采集对话框,ajax请求

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,42 +1,47 @@
 function preview() {
     $("#preview_area").html(UE.getEditor('editor').getContent());
 }
-function collect(srcUrl) {
-    var collectRequest = {
 
-    };
-    collectRequest.srcUrl = srcUrl;
-    $.ajax({
-        url : '/editor/collect',
-        type : 'POST',
-        data :  JSON.stringify(collectRequest),
-        contentType: "application/json",
-        dataType: 'json',
-        success : function (data) {
-            UE.getEditor('editor').setContent(data.htmlContent)
-        },
-        fail : function (xhr, textxStatus, errorThrown) {
-            alert("发布失败")
-        }
-    });
+function collect() {
+    srcUrl = document.getElementById("collect_input_url").value;
+    if (srcUrl !== undefined && srcUrl !== "" && srcUrl.match("^https://.*$")) {
+        $("#collectDialog").modal("hide");
+        var collectRequest = {};
+        collectRequest.srcUrl = srcUrl;
+        $.ajax({
+            url: '/editor/collect',
+            type: 'POST',
+            data: JSON.stringify(collectRequest),
+            contentType: "application/json",
+            dataType: 'json',
+            success: function (data) {
+                UE.getEditor('editor').setContent(data.htmlContent)
+            },
+            fail: function (xhr, textxStatus, errorThrown) {
+                alert("发布失败")
+            }
+        });
+    } else {
+        alert("无效的url地址")
+    }
 }
+
 function copy() {
     copyStringToClipboard(UE.getEditor('editor').getContent())
 }
-function release(content) {
-    var releaseRequest = {
 
-    };
+function release(content) {
+    var releaseRequest = {};
     releaseRequest.content = UE.getEditor('editor').getContent();
     $.ajax({
-        url : '/editor/release',
-        type : 'POST',
-        data :  JSON.stringify(releaseRequest),
+        url: '/editor/release',
+        type: 'POST',
+        data: JSON.stringify(releaseRequest),
         contentType: "application/json",
-        success : function (data) {
+        success: function (data) {
             alert("发布成功")
         },
-        fail : function (xhr, textxStatus, errorThrown) {
+        fail: function (xhr, textxStatus, errorThrown) {
             alert("发布失败")
         }
     });
